260f8c4d13c2c0d11e4750c92f9ca9a01462730d
Jean Sarmiento <jeancarlosr04@gmail.com>
2012-01-26 18:40:53 -0400

    Jean

--- Dec_Becerritos_app/Dec_Becerritos_app/Models/json.cs
+++ Dec_Becerritos_app/Dec_Becerritos_app/Models/json.cs
@@ -7,5 +7,36 @@ namespace Dec_Becerritos_app.Models
 {
     public class json
     {
+        public string Part1 = "apisandbox.junar.com/datastreams/invoke/";
+        public string GUID { get; set; }
+        public string Parameter { get; set; }
+        public string KEY = "auth_key=c8d666dcdf49f825f5edebb6041c52151d3b2bf6";
+
+        public string REGIONCIBAONORTE = "slw/01.sel";
+        public string ESPAILLAT = "slw/0109.sel";
+        public string PUERTOPLATA = "slw/0118.sel";
+        public string SANTIAGO = "slw/0125.sel";
+        public string REGIONCIBAOSUR = "slw/02.sel";
+        public string LAVEGA = "slw/0213.sel";
+        public string SANCHEZRAMIREZ = "slw/0224.sel";
+        public string MONSEÑORNOUEL = "slw/0228.sel";
+        public string REGIONCIBAONORDESTE = "slw/03.sel";
+        public string DUARTE = "slw/0306.sel";
+        public string MARIATRINIDADSANCHEZ = "slw/0314.sel";
+        public string SALCEDO = "slw/0319.sel";
+        public string SAMANA = "slw/0320.sel";
+        public string REGIONCIBAONOROESTE = "slw/04.sel";
+        public string DAJABON = "slw/0405.sel";
+        public string MONTECRISTI ="slw/0415.sel";
+        public string SANTIAGORODRIGUEZ = "slw/0426.sel";
+        public string VALVERDE = "slw/0427.sel";
+        public string REGIONVALDESIA = "slw/05.sel";
+        public string AZUA = "slw/0502.sel";
+        public string PERAVIA = "slw/0517.sel";
+        public string SANCRISTOBAL = "";
+
+
+
+
     }
 }
